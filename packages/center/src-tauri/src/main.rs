3ea7dc1bbbcc730bb2b03839613e36254674b78c
Danny <gh@hdcola.org>
2026-03-08 18:25:52 -0400

    format

--- packages/center/src-tauri/src/main.rs
+++ packages/center/src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows
-#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
 fn main() {
-  meetkey_center_lib::run();
+    meetkey_center_lib::run();
 }
